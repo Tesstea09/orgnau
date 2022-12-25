ba29be5c4f3523ab3697902247b25e29d1f0f419
Tesstea09 <cezar191299@gmail.com>
2022-12-26 00:13:04 +0200

    gdglmg

--- src/DetailEvent.js
+++ src/DetailEvent.js
@@ -12,7 +12,6 @@ import user from './icon/user.svg'
 import mail from './icon/mail.svg'
 import globe from './icon/globe.svg'
 import attach from './icon/file-attachment.svg'
-import Button from 'react-bootstrap/Button';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { useLocation } from "react-router-dom";
@@ -21,9 +20,15 @@ import { db } from "./firebase.js"
 import { doc, getDoc } from "@firebase/firestore";
 import { collection } from "firebase/firestore";
 
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import close from './close.svg'
 
 const DetailEvent = (props) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const { state } = useLocation();
   
     const [eventName, setEventName] = useState(null);
@@ -99,6 +104,7 @@ const DetailEvent = (props) => {
                         </div>
                         <div class = "but">
                             <div class="apply">
+                                <Button variant="primary" onClick={handleShow}>Launch static backdrop modal</Button>
                                 <a href="mailto:cezar191299@gmail.com?subject=Подача заявки">Подати заявку</a> 
                             </div>
                             <div class = "rulesb">
@@ -180,6 +186,59 @@ const DetailEvent = (props) => {
                 </div>
                 </div>
                 <Footer/>
+                
+        <Modal
+            dialogClassName = "rules"
+            show={show}
+            onHide={handleClose}
+            keyboard={true}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+
+        >
+            <Modal.Header>
+            <div class ="modalhead">
+            <Modal.Title id = "title">Правила подання та оформлення тез</Modal.Title>
+            <Button id = "modalclose" variant="secondary" onClick={handleClose}><img src={close}></img></Button>
+            </div>
+            </Modal.Header>
+            <Modal.Body>
+            <div>
+            <div class = "modalcontent">
+            <h1 class = "modalheader">Загальні вимоги до оформлення тез</h1>
+            <ul class = "modallist">
+                <li>Учасник може подати кілька доповідей;</li>
+                <li>Максимальна кількість авторів однієї доповіді – п'ять;</li>
+                <li>Об'єм матеріалів – від двох до десяти сторінок;</li>
+                <li>Формат тексту – Microsoft Word (*.doc, *.docx);</li>
+                <li>Мова тексту будь-яка – українська, англійська та інші;</li>
+                <li>Орієнтація сторінки – лише книжкова;</li>
+                <li>Поля (верхнє, нижнє, ліве, праве) – 2 см;</li>
+                <li>Шрифт - Times New Roman, кегель - 14;</li>
+                <li>Міжрядковий інтервал – одинарний, абзац – 0,75 див.</li>
+                <li>Колір шрифту – чорний.</li>
+            </ul>
+            </div>
+            <div class ="modalcontent">
+                <h1 class = "modalheader">Подання тез</h1>
+                <ul class = "modallist">
+                <li>Одним листом надсилаються файли тез доповідей та квитанція про внесення оргвнеску на ел. пошту: info@isg-konf.com.</li>
+                <li>Тема листа – назва конференції.</li>
+                <li>Файли назвати за прикладом: Тези_Петров; Оргвнесок_Петров.</li>
+                <li>Формат тексту – Microsoft Word (*.doc, *.docx);</li>
+                <li>Після надсилання матеріалів на нашу ел. пошту чекайте на підтвердження про їх отримання та включення вас до списку учасників конференції.</li>
+                <li>Організаційний внесок для участі у конференції вказується на сторінці самої конференції та в інформаційному листі конференції.</li>
+                </ul>
+            </div>
+            </div>
+            </Modal.Body>
+            <Modal.Footer >
+            <div class="ex">
+                <a href={logo} download="newfile.png"><Button id = "certificate" variant="general" >Приклад сертифікату</Button></a>
+                <a href={logo} download="newfile.png"><Button id = "example" variant="general" >Приклад оформлення тез</Button></a>
+            </div>
+            </Modal.Footer>
+        </Modal>
             </div>
         )
 
