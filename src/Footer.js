3aef0e963503a0a3245386001ce449a63501192d
Tesstea09 <cezar191299@gmail.com>
2022-12-26 02:48:49 +0200

    dddhhgk

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,18 @@
 import React,{Component} from 'react'
 import './Ent.css'
 import logo from './logo.png'
+import { Link } from "react-router-dom";
+
 function Footer(){
         return(
             <div class="footer">
                 <div class = "FirstPF">
-                    <img src={logo} alt=""></img>
-                    <a href="">Всі конференції</a>
+                    <Link to = '/EventList'>
+                        <img src={logo} alt=""></img>
+                    </Link>
+                    <Link to = '/EventList'>
+                        <a href="">Всі конференції</a>
+                    </Link>
                 </div>
                 <div class = "SecondPF"> 
                     <p>© 2022</p>
